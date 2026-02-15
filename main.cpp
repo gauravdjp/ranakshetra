625ad3a6545027fdd847309c06d44b2de1a3e954
Gaurav <gdpatil1566@gmail.com>
2026-02-15 17:09:36 +0530

    added new function greet

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <string>
+int greet(string name){
+	std::cout<<"hello sir, "<< name << endl;
+}
+
 int main(){
 	std::cout << "hello world!!" << endl;
+	greet("odis");
 	return 0;
 }
 			
