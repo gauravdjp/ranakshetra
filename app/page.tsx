99860e7d0bd1e17b1d369e828b775c79d88fe097
Gaurav <gdpatil1566@gmail.com>
2026-02-28 16:13:22 +0530

    modified (readme, app/page.tsx, package-lock.json, package.json), added lib and types folder, created and connected the mongodb to our app and successfully tested!

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,29 @@
-import Image from "next/image";
+import { Admin } from '../types';
+import { WithId } from 'mongodb';
+import clientPromise from '@/lib/mongodb';
 
-export default function Home() {
+export default async function Home() {
+  const client = await clientPromise;
+  const db = client.db('rks');
+
+  await db.collection<Admin>('admins').insertOne({
+    username: 'admin',
+    password: 'password',
+    role: 'admin',
+    createdAt: new Date(),
+  });
+
+  const admins: WithId<Admin>[] = await db.collection<Admin>('admins').find({}).toArray();
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1>Admins</h1>
+      {admins.map((admin) => (
+        <div key={admin._id.toString()}>
+          <p>{admin.username}</p>
+          <p>{admin.role}</p>
+        </div>
+      ))}
     </div>
   );
 }
---
+++ types/index.ts
@@ -0,0 +1,6 @@
+export interface Admin{
+    username : string;
+    password : string;
+    role : 'admin';
+    createdAt : Date;
+}
