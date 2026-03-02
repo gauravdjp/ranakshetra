e23f6e64b852d25ec0ae5639edbebe9c019f4e33
Gaurav <gdpatil1566@gmail.com>
2026-03-02 19:36:03 +0530

    added new pages and started making components

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,11 @@
-import { Admin } from '../types';
-import { WithId } from 'mongodb';
-import clientPromise from '@/lib/mongodb';
-
 export default async function Home() {
-  const client = await clientPromise;
-  const db = client.db('rks');
-
-  await db.collection<Admin>('admins').insertOne({
-    username: 'admin',
-    password: 'password',
-    role: 'admin',
-    createdAt: new Date(),
-  });
-
-  const admins: WithId<Admin>[] = await db.collection<Admin>('admins').find({}).toArray();
+  
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <h1>Admins</h1>
-      {admins.map((admin) => (
-        <div key={admin._id.toString()}>
-          <p>{admin.username}</p>
-          <p>{admin.role}</p>
-        </div>
-      ))}
-    </div>
+    <>
+      <header>
+        <h1>RANAKSHETRA</h1>
+        
+      </header>
+    </>
   );
 }
