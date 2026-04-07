ff13c961b2d170ba3148caf47911fd783edc36d0
Gaurav <gdpatil1566@gmail.com>
2026-04-07 22:09:20 +0530

    added types

--- types/index.ts
+++ types/index.ts
@@ -1,18 +1,35 @@
-export interface Organiser{
-    username : string;
-    password : string;
-    email : string;
-    arena_name : string;
-    country : string;
-    region : string;
-    role : 'organiser';
-    created_at : Date;
+export type Basic_Info = {
+    readonly name : string,
+    readonly email : string,
+    readonly phone? : number,
+    readonly created_at : Date,
+    readonly updated_at : Date,
 }
 
-export interface participant{}
+export enum Access_Level{
+    USER = "user",
+    ADMIN = "admin",
+}
 
-export interface club{}
+export enum Access_Level_USER_Role{
+    PLAYER = "player",
+    ORGANISER = "organiser",
+}
 
-export interface tournament{}
+export enum Access_Level_ADMIN_Role{
+    DEVELOPER = "developer",
+}
+
+export type Organiser = Basic_Info & {
+    _id? : string,
+    username : string,
+    password : string,
+    arena_name : string,
+    arena_location : string,
+    arena_description? : string,
+    arena_image? : string,
+    is_verified : boolean,
+  
+}
 
 
